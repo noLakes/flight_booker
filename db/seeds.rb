27f12ef45d94cb9fd59f88cb8f9278e57844b283
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-30 22:53:56 -0400

    added seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+alpha = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"].split('')
+
+def code_gen
+  result = []
+  3.times { result << alpha.sample}
+  result.join('')
+end
+
+def rand_time
+  a = DateTime.now.to_i
+  b = a + 500
+  Time.at(rand(a..b))
+end
+
+10.times do 
+  Airport.create(code: code_gen)
+end
+
+10.times do
+  Flight.create(departure_airport_id: Airport.all.sample.id,
+  arrival_airport_id: Airport.all.sample.id,
+  time: rand_time,
+  duration: rand(1..14))
+end
+
