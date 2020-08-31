6e5e109cddda788d24c8a2faebc23d8ceb0190de
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-30 20:07:41 -0400

    added associations between airports and flights

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,4 @@
 class Airport < ApplicationRecord
+  has_many :departure_flights, class_name: 'Flight', foreign_key: 'departure_airport_id'
+  has_many :arrival_flights, class_name: 'Flight', foreign_key: 'arrival_airport_id'
 end
