d341516d25bd0e31d47ef60c8402ca3ba3675e29
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-30 23:20:36 -0400

    added method scaffold to flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,20 @@
 class FlighsController < ApplicationController
 
-  
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+
+  def flight_params
+
+  end
 end
