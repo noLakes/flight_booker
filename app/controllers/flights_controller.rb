8b0196af9b444d45ffc535fc7cd637b77e396834
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-31 21:05:40 -0400

    added strong params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -20,7 +20,7 @@ class FlightsController < ApplicationController
   private
 
   def flight_params
-
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :time, :passengers, :duration)
   end
 
 end
