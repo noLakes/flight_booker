481f94d1a19b6d9be56e259d0416acc4a78ab924
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-03 19:18:33 -0400

    added hidden passenger_count to booking submit form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,19 @@
 class BookingsController < ApplicationController
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
   end
 
   def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit()
   end
 end
