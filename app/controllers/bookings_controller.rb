404058e5b165b7afba8a56297f650f5385d301ab
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-05 23:42:57 -0400

    removed @p_num variable

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,7 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find(booking_params[:flight_id])
-    @p_num = booking_params[:passenger_count].to_i
-    @p_num.times { @booking.passengers.build }
+    booking_params[:passenger_count].to_i.times { @booking.passengers.build }
 
     
   end
