23b412d97569852bf4e0efdfdf230949f4240053
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-14 23:06:17 -0400

    changed mailer defaults

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,7 +2,7 @@ class PassengerMailer < ApplicationMailer
 
   def thank_you_email
     @passenger = params[:passenger]
-    @url = 'http://example.com/login'
+    @url = 'http://flightbooker.com/login'
     mail(to: @passenger.email, subject: "Thank you for booking with Flight Booker!")
   end
 end
